f6424b403c1457fbf02124a64bde75ddea067262
EderNatan.dev <edernatan124@gmail.com>
2024-07-31 01:21:05 -0300

    ajust layot

--- PlugInfo2024.1/5-Blog/blog-prismic/src/app/layout.tsx
+++ PlugInfo2024.1/5-Blog/blog-prismic/src/app/layout.tsx
@@ -1,23 +1,25 @@
-import { PrismicPreview } from '@prismicio/next'
-import { repositoryName } from '@/prismicio'
-import "./globals.css";
-import { Montserrat } from "next/font/google";
+import { PrismicPreview } from '@prismicio/next';
+import { repositoryName } from '@/prismicio';
+import './globals.css';
+import { Montserrat } from 'next/font/google';
+import { ReactNode } from 'react';
 
-
-const montserrat = Montserrat ({ 
+const montserrat = Montserrat({
   weight: ['500'],
-  subsets: ["latin"] 
+  subsets: ['latin']
 });
 
+interface RootLayoutProps {
+  children: ReactNode;
+}
 
-
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
-      <body className={montserrat.className} >
+      <body className={montserrat.className}>
         {children}
         <PrismicPreview repositoryName={repositoryName} />
       </body>
     </html>
-  )
+  );
 }
